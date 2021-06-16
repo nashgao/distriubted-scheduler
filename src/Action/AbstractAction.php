fcea5760e8c49230c068888d8d1e5826143ac46c
nashgao <nashgao1992@gmail.com>
2021-06-16 14:20:53 +1000

    update

--- src/Action/AbstractAction.php
+++ src/Action/AbstractAction.php
@@ -6,7 +6,7 @@ namespace Nashgao\DistributedScheduler\Action;
 
 class AbstractAction
 {
-    public ?string $schedulerClass = null;
+    public string $schedulerClass;
 
     public function setSchedulerClass(string $schedulerClass): AbstractAction
     {
